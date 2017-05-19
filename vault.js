6d740bfeebb866928b38566e805b31213826c6f8
Joshua Lee <jdl93@Joshuas-MacBook-Air.local>
2017-05-18 19:45:18 -1000

    I am lost

--- vault.js
+++ vault.js
@@ -1,4 +1,22 @@
-'use strict';
+//'use strict';
 module.exports = function() {
-  
+
+  function setvalue(key, value){
+
+
+  }
+
+
+  function getValue(key){
+    if( key === value){
+      return value;
+    } else {
+      return null;
+    }
+  }
+  return {
+    setvalue: setvalue,
+    getValue: getValue
+  };
+
 };
